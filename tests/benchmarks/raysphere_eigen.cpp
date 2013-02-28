233a02a33dc2ae7d231e8261c1e59017de5ec8dc
James Hegarty <jameshegarty@gmail.com>
2013-02-27 20:48:55 -0800

    added bs and raysphere in eigen

---
+++ tests/benchmarks/raysphere_eigen.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+#include <float.h>
+
+#include "timing.h"
+
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+int main() {
+
+	int n = 10000000;
+	double xo = 0;
+	double yo = 0;
+	double zo = 0;
+	double xd = 1;
+	double yd = 0;
+	double zd = 0;
+	
+  ArrayXd xc(n);
+  ArrayXd yc(n);
+  ArrayXd zc(n);
+
+	for(int i = 0; i < n; i++) {
+		xc[i] = i;
+		yc[i] = i;
+		zc[i] = i;
+	}
+
+  ArrayXd b(n);
+  ArrayXd c(n);
+  ArrayXd disc(n);
+	b.fill(0);
+	c.fill(0);
+	disc.fill(0);
+
+	double begin = current_time();
+
+	double a = 1;
+		
+	double r = DBL_MAX;
+	
+  b = 2*(xd*(xo-xc)+yd*(yo-yc)+zd*(zo-zc));
+  c = (xo-xc)*(xo-xc)+(yo-yc)*(yo-yc)+(zo-zc)*(zo-zc)-1;
+  disc = b*b-4*c;
+  r = ((disc>0).select(((-b - disc.sqrt())/2).min( (-b + disc.sqrt())/2),DBL_MAX)).minCoeff();
+
+	printf("%f\n",r);
+	printf("Elapsed: %f\n", current_time()-begin);
+	return 0;
+}
---
+++ tests/benchmarks/timing.h
@@ -0,0 +1,8 @@
+
+#include <sys/time.h>
+
+double current_time() {
+	struct timeval v;
+	gettimeofday(&v, NULL);
+	return v.tv_sec +  v.tv_usec / 1000000.0;
+}
