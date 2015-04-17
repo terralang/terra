66adda4a3065e72314e26358b77b424244b54566
Zachary DeVito <zdevito@stanford.edu>
2015-04-17 15:17:05 -0700

    distinguish between unpackstruct and unpacktuple. unpack tuple will only unpack structs created from the tuple constructor.

--- src/terralib.lua
+++ src/terralib.lua
@@ -3671,36 +3671,43 @@ _G["arrayof"] = terra.internalmacro(function(diag,tree,typ,...)
     local exps = terra.newlist({...}):map(function(x) return x.tree end)
     return terra.newtree(tree, { kind = terra.kinds.arrayconstructor, oftype = typ:astype(), expressions = exps })
 end)
-terra.unpackstruct = terra.internalmacro(function(diag,tree,obj)
-    local typ = obj:gettype()
-    if not obj or not typ:isstruct() or typ.convertible ~= "tuple" then
-        return obj
-    end
-    if not obj:islvalue() then diag:reporterror("expected an lvalue") end
-    local result = terralib.newlist()
-    for i,e in ipairs(typ:getentries()) do 
-        if e.field then
-            result:insert(terra.newtree(tree, {kind = terra.kinds.select, field = e.field, value = obj.tree }))
+
+local function createunpacks(tupleonly)
+    local function unpackterra(diag,tree,obj)
+        local typ = obj:gettype()
+        if not obj or not typ:isstruct() or (tupleonly and typ.convertible ~= "tuple") then
+            return obj
         end
-    end
-    return result
-end,
-function(cdata)
-    local t = type(cdata) == "cdata" and terra.typeof(cdata)
-    if not t or not t:isstruct() or t.convertible ~= "tuple" then 
-      return cdata
-    end
-    local results = terralib.newlist()
-    for i,e in ipairs(t:getentries()) do
-        if e.field then
-            local nm = terra.issymbol(e.field) and e.field:tocname() or e.field
-            results:insert(cdata[nm])
+        if not obj:islvalue() then diag:reporterror("expected an lvalue") end
+        local result = terralib.newlist()
+        for i,e in ipairs(typ:getentries()) do 
+            if e.field then
+                result:insert(terra.newtree(tree, {kind = terra.kinds.select, field = e.field, value = obj.tree }))
+            end
         end
+        return result
     end
-    return unpack(results)
-end)
+    local function unpacklua(cdata)
+        local t = type(cdata) == "cdata" and terra.typeof(cdata)
+        if not t or not t:isstruct() or (tupleonly and t.convertible ~= "tuple") then 
+          return cdata
+        end
+        local results = terralib.newlist()
+        for i,e in ipairs(t:getentries()) do
+            if e.field then
+                local nm = terra.issymbol(e.field) and e.field:tocname() or e.field
+                results:insert(cdata[nm])
+            end
+        end
+        return unpack(results)
+    end
+    return unpackterra,unpacklua
+end
+terra.unpackstruct = terra.internalmacro(createunpacks(false))
+terra.unpacktuple = terra.internalmacro(createunpacks(true))
+
 _G["unpackstruct"] = terra.unpackstruct
-_G["unpacktuple"] = terra.unpackstruct
+_G["unpacktuple"] = terra.unpacktuple
 _G["tuple"] = terra.types.tuple
 _G["global"] = terra.global
 
