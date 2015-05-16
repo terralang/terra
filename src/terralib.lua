37843b4cf24533c540eef0191a06fa39c9485aaf
Zachary DeVito <zdevito@stanford.edu>
2015-05-16 15:45:17 -0700

    don't raise an error in compiler when we try to use a callback luajit can't do yet.

--- src/terralib.lua
+++ src/terralib.lua
@@ -2982,8 +2982,11 @@ function terra.funcdefinition:typecheck()
             local varargslist = paramlist:map(function(p) return "vararg" end)
             paramlist = tryinsertcasts(anchor, terra.newlist{varargslist},castbehavior, false, false, paramlist)
             local castedtype = terra.types.funcpointer(paramlist:map("type"),{})
-            local cb = terra.cast(castedtype,fn)
-            local fptr = terra.pointertolightuserdata(cb)
+            local success, cb = pcall(function() return terra.cast(castedtype,fn) end)
+            if not success then
+                diag:reporterror(anchor, "unsupported callback function type: ", castedtype)
+            end
+            local fptr = cb and terra.pointertolightuserdata(cb)
             return terra.newtree(anchor, { kind = terra.kinds.luafunction, callback = cb, fptr = fptr, type = castedtype }),paramlist
         end
         
