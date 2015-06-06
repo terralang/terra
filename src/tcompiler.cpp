2c552a6240865bac0b7c625a34524717678ffd42
Zachary DeVito <zdevito@stanford.edu>
2015-06-05 20:44:17 -0700

    generate nounwind attributes so that arm doesn't make exception tables.

--- src/tcompiler.cpp
+++ src/tcompiler.cpp
@@ -900,6 +900,9 @@ struct CCallingConv {
         Classification * info = ClassifyFunction(ftype);
         Function * fn = Function::Create(info->fntype, Function::ExternalLinkage, name, M);
         AttributeFnOrCall(fn,info);
+        #if LLVM_VERSION > 32
+            fn->addAttribute(llvm::AttributeSet::FunctionIndex, llvm::Attribute::NoUnwind);
+        #endif
         return fn;
     }
     
--- src/tllvmutil.cpp
+++ src/tllvmutil.cpp
@@ -274,6 +274,7 @@ void llvmutil_optimizemodule(Module * M, TargetMachine * TM) {
     PMB.OptLevel = 3;
     PMB.SizeLevel = 0;
     PMB.Inliner = createFunctionInliningPass(PMB.OptLevel, 0);
+    
 #if LLVM_VERSION >= 35
     PMB.LoopVectorize = true;
     PMB.SLPVectorize = true;
