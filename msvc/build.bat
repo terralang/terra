5f9540fd78863dc635043d7fab7ccd4c9c32177f
Michael <mikemx7f@gmail.com>
2018-10-22 17:24:36 -0700

    Taking a crack at building terra on windows VS2015+clang6.0.1

--- msvc/build.bat
+++ msvc/build.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 call nmake
 echo Done.
 pause >nul
