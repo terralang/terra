72d7b912cac349ac237175b7a11e3276027ee513
Michael <mikemx7f@gmail.com>
2018-10-29 15:24:27 -0700

    Moved clangpaths in front of terrapaths, removed pause at end of windows build script, added workaround for implicit size_t definition on windows after llvm 3.5. Removed llvm3.8+VS2015 from the build matrix (since it isn't supported).

--- msvc/build.bat
+++ msvc/build.bat
@@ -3,4 +3,3 @@ if "%VS_MINOR_VERSION%"=="" set VS_MINOR_VERSION=0
 call "C:\Program Files (x86)\Microsoft Visual Studio %VS_MAJOR_VERSION%.%VS_MINOR_VERSION%\VC\vcvarsall.bat" x86_amd64
 call nmake
 echo Done.
-pause >nul
