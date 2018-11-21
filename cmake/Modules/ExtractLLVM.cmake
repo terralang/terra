a1dfd50b9b09e639a678eec0382cacf48934b825
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-11-21 10:32:23 -0800

    Support for dynamically linking LLVM.

--- cmake/Modules/ExtractLLVM.cmake
+++ cmake/Modules/ExtractLLVM.cmake
@@ -1,17 +1,3 @@
-set(LLVM_OBJECT_DIR "${PROJECT_BINARY_DIR}/llvm_objects")
-
-execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E make_directory "${LLVM_OBJECT_DIR}"
-)
-
-foreach(LLVM_LIB ${LLVM_AVAILABLE_LIBS})
-  get_property(LLVM_LIB_TYPE TARGET ${LLVM_LIB} PROPERTY TYPE)
-  if(${LLVM_LIB_TYPE} STREQUAL STATIC_LIBRARY)
-    get_property(LLVM_LIB TARGET ${LLVM_LIB} PROPERTY LOCATION)
-    list(APPEND LLVM_LIBRARIES ${LLVM_LIB})
-  endif()
-endforeach()
-
 if("${LLVM_DEFINITIONS}" STREQUAL "@LLVM_DEFINITIONS@")
   # Some versions of LLVM produce broken CMake configurations, so
   # provide default definitions when this is the case.
@@ -27,31 +13,63 @@ else()
   list(APPEND ALL_LLVM_DEFINITIONS ${LLVM_DEFINITIONS_LIST})
 endif()
 
-foreach(LLVM_LIB_PATH ${LLVM_LIBRARIES} ${CLANG_LIBRARIES})
-  get_filename_component(LLVM_LIB_NAME "${LLVM_LIB_PATH}" NAME)
+if(TERRA_STATIC_LINK_LLVM)
+  set(LLVM_OBJECT_DIR "${PROJECT_BINARY_DIR}/llvm_objects")
+
   execute_process(
-    COMMAND "${CMAKE_AR}" t "${LLVM_LIB_PATH}"
-    OUTPUT_VARIABLE LLVM_LIB_CONTENTS
+    COMMAND "${CMAKE_COMMAND}" -E make_directory "${LLVM_OBJECT_DIR}"
   )
-  string(REGEX MATCHALL "[^\n]+" LLVM_LIB_OBJECT_BASENAMES "${LLVM_LIB_CONTENTS}")
-  unset(LLVM_OBJECTS)
-  foreach(LLVM_OBJECT ${LLVM_LIB_OBJECT_BASENAMES})
-    if(${LLVM_OBJECT} MATCHES \.o$)
-      list(APPEND LLVM_OBJECTS "${LLVM_OBJECT_DIR}/${LLVM_LIB_NAME}/${LLVM_OBJECT}")
+
+  foreach(LLVM_LIB ${LLVM_AVAILABLE_LIBS})
+    get_property(LLVM_LIB_TYPE TARGET ${LLVM_LIB} PROPERTY TYPE)
+    if(${LLVM_LIB_TYPE} STREQUAL STATIC_LIBRARY)
+      get_property(LLVM_LIB TARGET ${LLVM_LIB} PROPERTY LOCATION)
+      list(APPEND LLVM_LIBRARIES ${LLVM_LIB})
     endif()
   endforeach()
-  execute_process(
-    COMMAND "${CMAKE_COMMAND}" -E make_directory "${LLVM_OBJECT_DIR}/${LLVM_LIB_NAME}"
-  )
-  add_custom_command(
-    OUTPUT ${LLVM_OBJECTS}
-    DEPENDS ${LLVM_LIB_PATH}
-    COMMAND "${CMAKE_AR}" x "${LLVM_LIB_PATH}"
-    WORKING_DIRECTORY "${LLVM_OBJECT_DIR}/${LLVM_LIB_NAME}"
-    VERBATIM
-  )
-  list(APPEND ALL_LLVM_OBJECTS ${LLVM_OBJECTS})
-endforeach()
+
+  foreach(LLVM_LIB_PATH ${LLVM_LIBRARIES} ${CLANG_LIBRARIES})
+    get_filename_component(LLVM_LIB_NAME "${LLVM_LIB_PATH}" NAME)
+    execute_process(
+      COMMAND "${CMAKE_AR}" t "${LLVM_LIB_PATH}"
+      OUTPUT_VARIABLE LLVM_LIB_CONTENTS
+    )
+    string(REGEX MATCHALL "[^\n]+" LLVM_LIB_OBJECT_BASENAMES "${LLVM_LIB_CONTENTS}")
+    unset(LLVM_OBJECTS)
+    foreach(LLVM_OBJECT ${LLVM_LIB_OBJECT_BASENAMES})
+      if(${LLVM_OBJECT} MATCHES \.o$)
+        list(APPEND LLVM_OBJECTS "${LLVM_OBJECT_DIR}/${LLVM_LIB_NAME}/${LLVM_OBJECT}")
+      endif()
+    endforeach()
+    execute_process(
+      COMMAND "${CMAKE_COMMAND}" -E make_directory "${LLVM_OBJECT_DIR}/${LLVM_LIB_NAME}"
+    )
+    add_custom_command(
+      OUTPUT ${LLVM_OBJECTS}
+      DEPENDS ${LLVM_LIB_PATH}
+      COMMAND "${CMAKE_AR}" x "${LLVM_LIB_PATH}"
+      WORKING_DIRECTORY "${LLVM_OBJECT_DIR}/${LLVM_LIB_NAME}"
+      VERBATIM
+    )
+    list(APPEND ALL_LLVM_OBJECTS ${LLVM_OBJECTS})
+  endforeach()
+
+  # Don't link libraries, since we're using the extracted object files.
+  list(APPEND ALL_LLVM_LIBRARIES)
+else()
+  foreach(LLVM_LIB ${LLVM_AVAILABLE_LIBS})
+    get_property(LLVM_LIB_TYPE TARGET ${LLVM_LIB} PROPERTY TYPE)
+    if(${LLVM_LIB_TYPE} STREQUAL SHARED_LIBRARY)
+      list(APPEND ALL_LLVM_LIBRARIES ${LLVM_LIB})
+    endif()
+  endforeach()
+
+  # For now, statically link Clang.
+  list(APPEND ALL_LLVM_LIBRARIES ${CLANG_LIBRARIES})
+
+  # Don't extract individual object files.
+  list(APPEND ALL_LLVM_OBJECTS)
+endif()
 
 add_custom_target(
   LLVMObjectFiles
@@ -69,3 +87,8 @@ foreach(LLVM_LIB ${LLVM_AVAILABLE_LIBS})
 endforeach()
 list(REMOVE_ITEM LLVM_SYSTEM_LIBRARIES ${LLVM_AVAILABLE_LIBS})
 list(REMOVE_DUPLICATES LLVM_SYSTEM_LIBRARIES)
+
+mark_as_advanced(
+  ALL_LLVM_LIBRARIES
+  ALL_LLVM_OBJECTS
+)
