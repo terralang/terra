a84bb4e656f7015b87026db2d8d63cc714d9a9d1
Elliott Slaughter <elliottslaughter@gmail.com>
2019-02-19 14:29:12 -0800

    Add support for LLVM 7.0 with CUDA (#352)
    * Add LLVM 7.0 headers
    
    * Fix WriteBitcodeToFile
    
    * API upgrade to 7.0
    
    * Add LLVM 7.0 to Travis.
    
    * Fix typo in Travis.
    
    * Fix LLVM 7 path in Travis.
    
    * Set CMake to default to not including LLVM in static binaries with LLVM >= 7.
    
    * Update supported LLVM versions.
    
    * Simplify LLVM 7 code changes.

--- src/tllvmutil.cpp
+++ src/tllvmutil.cpp
@@ -191,9 +191,16 @@ bool llvmutil_emitobjfile(Module * Mod, TargetMachine * TM, bool outputobjectfil
     #else
     emitobjfile_t & destf = dest;
     #endif
+
+    #if LLVM_VERSION >= 70
+    if (TM->addPassesToEmitFile(pass, destf, nullptr, ft)) {
+        return true;
+    }
+    #else
     if (TM->addPassesToEmitFile(pass, destf, ft)) {
         return true;
     }
+    #endif
 
     pass.run(*Mod);
     
@@ -403,7 +410,11 @@ error_code llvmutil_createtemporaryfile(const Twine &Prefix, StringRef Suffix, S
 int llvmutil_executeandwait(LLVM_PATH_TYPE program, const char ** args, std::string * err) {
 #if LLVM_VERSION >= 34
     bool executionFailed = false;
+    #if LLVM_VERSION >= 70
+    llvm::sys::ProcessInfo Info = llvm::sys::ExecuteNoWait(program, llvm::toStringRefArray(args), llvm::None, {}, 0, err, &executionFailed);
+    #else
     llvm::sys::ProcessInfo Info = llvm::sys::ExecuteNoWait(program,args,nullptr,{},0,err,&executionFailed);
+    #endif
     if(executionFailed)
         return -1;
     #ifndef _WIN32
