d1792ae843d1a954a11e74a4e8a4a9de3e40ac61
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-08-20 15:17:04 -0700

    Remove Opt from Travis for now.

--- travis.sh
+++ travis.sh
@@ -153,14 +153,4 @@ elif [[ $EXTERNAL_TEST = rigel ]]; then
     ln -s "$TERRA_INSTALL_PREFIX/../build/bin/luajit" bin/luajit
     export PATH="$PATH:$TERRA_INSTALL_PREFIX/bin:$PWD/bin"
     make terra
-elif [[ $EXTERNAL_TEST = opt ]]; then
-    git clone https://github.com/niessner/Opt.git opt
-    export TERRAHOME=$TERRA_INSTALL_PREFIX
-    export TERRA=$TERRA_INSTALL_PREFIX/bin/terra
-    pushd opt/API
-    make
-    popd
-    pushd opt/examples/image_warping
-    make CXX=$($LLVM_CONFIG --bindir)/clang++ LINK=$($LLVM_CONFIG --bindir)/clang++
-    popd
 fi
