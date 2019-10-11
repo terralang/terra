3a51676385e62d5e41717e3de4284109d97986d3
Erik McClure <erikm@blackspherestudios.com>
2019-10-10 17:54:55 -0700

    Add proper versioning and pass architecture to getvclinker()

--- src/terralib.lua
+++ src/terralib.lua
@@ -4043,12 +4043,16 @@ end
 
 
 -- configure path variables
-terra.cudahome = os.getenv("CUDA_HOME") or (ffi.os == "Windows" and os.getenv("CUDA_PATH")) or "/usr/local/cuda"
+if ffi.os == "Windows" then
+  terra.cudahome = os.getenv("CUDA_PATH")
+else
+  terra.cudahome = os.getenv("CUDA_HOME") or "/usr/local/cuda"
+end
 terra.cudalibpaths = ({ OSX = {driver = "/usr/local/cuda/lib/libcuda.dylib", runtime = "$CUDA_HOME/lib/libcudart.dylib", nvvm =  "$CUDA_HOME/nvvm/lib/libnvvm.dylib"}; 
                        Linux =  {driver = "libcuda.so", runtime = "$CUDA_HOME/lib64/libcudart.so", nvvm = "$CUDA_HOME/nvvm/lib64/libnvvm.so"}; 
                        Windows = {driver = "nvcuda.dll", runtime = "$CUDA_HOME\\bin\\cudart64_*.dll", nvvm = "$CUDA_HOME\\nvvm\\bin\\nvvm64_*.dll"}; })[ffi.os]
 -- OS's that are not supported by CUDA will have an undefined value here
-if terra.cudalibpaths then
+if terra.cudalibpaths and terra.cudahome then
 	for name,path in pairs(terra.cudalibpaths) do
 		path = path:gsub("%$CUDA_HOME",terra.cudahome)
 		if path:match("%*") and ffi.os == "Windows" then
@@ -4074,13 +4078,29 @@ if ffi.os == "Windows" then
         end
         terra.includepath = os.getenv("INCLUDE")
       
-        function terra.getvclinker()
+        function terra.getvclinker(target)
           local vclib = os.getenv("LIB")
           local vcpath = terra.vcpath or os.getenv("Path")
           vclib,vcpath = "LIB="..vclib,"Path="..vcpath
           return terra.vclinker,vclib,vcpath
         end
     else
+        local function compareversion(form, a, b)
+          if (a == nil) or (b == nil) then return true end
+          
+          local alist = {}
+          for e in string.gmatch(a, form) do table.insert(alist, tonumber(e)) end
+          local blist = {}
+          for e in string.gmatch(b, form) do table.insert(blist, tonumber(e)) end
+          
+          for i=1,#alist do
+            if alist[i] ~= blist[i] then
+              return alist[i] > blist[i]
+            end
+          end
+          return false
+        end
+        
         -- First find the latest Windows SDK installed using the registry
         local installedroots = [[SOFTWARE\Microsoft\Windows Kits\Installed Roots]]
         local windowsdk = terra.queryregvalue(installedroots, "KitsRoot10")
@@ -4092,15 +4112,28 @@ if ffi.os == "Windows" then
           
           terra.systemincludes:insertall{ "shared", "um", "winrt" }
           terra.systemincludes = terra.systemincludes:map(function(e) return windowsdk..[[include\]]..e end)
-          terra.sdklib = terra.listsubdirectories(windowsdk .. "lib")[1] -- Just use the first folder we find
-        else
-          local function compareversion(a, b) return true end
-          -- Find highest version. For sanity reasons, we assume the same version folders are in both lib/ and include/
-          local version = "0.0.0.0"
-          for i, v in ipairs(terra.listsubdirectories(windowsdk .. "include")) do
-            if compareversion(v, version) then
+          
+          local version = nil
+          for i, v in ipairs(terra.listsubdirectories(windowsdk .. "lib")) do
+            if compareversion("%d+", v, version) then
               version = v
             end
+          end
+          if version == nil then
+            error "Can't find valid version subdirectory in the SDK! Is the Windows 8.1 SDK installation corrupt?"
+          end
+          
+          terra.sdklib = windowsdk .. "lib\\" .. version
+        else
+          -- Find highest version. For sanity reasons, we assume the same version folders are in both lib/ and include/
+          local version = nil
+          for i, v in ipairs(terra.listsubdirectories(windowsdk .. "include")) do
+            if compareversion("%d+", v, version) then
+              version = v
+            end
+          end
+          if version == nil then
+            error "Can't find valid version subdirectory in the SDK! Is the SDK installation corrupt?"
           end
           
           terra.systemincludes:insertall{ "ucrt", "shared", "um", "winrt" }
@@ -4134,8 +4167,8 @@ if ffi.os == "Windows" then
             ([[%sATLMFC\INCLUDE]]):format(terra.vshome)
         }
         
-        function terra.getvclinker() --get the linker, and guess the needed environment variables for Windows if they are not set ...
-            local target = "x86_64" -- TODO: pass into getvclinker()
+        function terra.getvclinker(target) --get the linker, and guess the needed environment variables for Windows if they are not set ...
+            target = target or "x86_64"
             local winarch = target
             if target == "x86_64" then
               target = terra.vsarch64
