d3e5841529c3c8e257aa30e4ac6b45ee5422c6a9
Elliott Slaughter <slaughter@cs.stanford.edu>
2020-04-21 15:09:01 -0700

    Go back to old include path.

--- travis.sh
+++ travis.sh
@@ -143,8 +143,8 @@ if [[ $(uname) = Darwin ]]; then
 
   # workaround for https://github.com/terralang/terra/issues/365
   # xcode-select --install
-  # export INCLUDE_PATH="$(xcrun --sdk macosx --show-sdk-path)/usr/include"
-  export INCLUDE_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include
+  export INCLUDE_PATH="$(xcrun --sdk macosx --show-sdk-path)/usr/include"
+  # export INCLUDE_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include
 
   export PATH=$PWD:$PATH
 fi
