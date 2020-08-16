785b2601a7bb22857dc71533ca96d0515ce95f42
Erik McClure <erikm@fundament.software>
2020-08-15 18:59:15 -0700

    Fix clang format whining

--- src/twindows.h
+++ src/twindows.h
@@ -3,7 +3,7 @@
 
 #pragma pack(push)
 #pragma pack(8)
-#define WINVER 0x0600  //_WIN32_WINNT_VISTA 
+#define WINVER 0x0600  //_WIN32_WINNT_VISTA
 #define _WIN32_WINNT 0x0600
 #define NTDDI_VERSION 0x06000100  // NTDDI_VISTASP1
 #define WIN32_LEAN_AND_MEAN
