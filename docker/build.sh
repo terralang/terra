78fcdf61297ea7c04835fd0936702a80fad05920
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-10-05 13:16:48 -0700

    Test LLVM 3.8 via Docker since it's not available in newer Ubuntu (#512)

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,7 @@
 set -e
 
 IFS=- read distro release <<< "$1"
+llvm="$2"
+variant="$3"
 
-docker build --build-arg release=$release -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro .
+docker build --build-arg release=$release --build-arg llvm=$llvm -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro${variant:+-}$variant .
