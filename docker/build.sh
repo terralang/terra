6c4331e74df3550ecfb1bf7f1a470c1937167138
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-10-05 12:24:24 -0700

    Fix typo.

--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,4 @@ IFS=- read distro release <<< "$1"
 llvm="$2"
 variant="$3"
 
-docker build --build-arg release=$release --build-arg llvm=$llvm -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro${variant+-}$variant .
+docker build --build-arg release=$release --build-arg llvm=$llvm -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro${variant:+-}$variant .
