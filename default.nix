4d6704526e43c44853f4f1732d49299a18d8def3
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-04-19 10:29:44 -0700

    Attempt to fix the broken Nix build.

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ let
 
   llvmPackages = pkgs.llvmPackages_10;
   stdenv = llvmPackages.stdenv;
-  cuda = cudaPackages.cudatoolkit_11;
+  cuda = if cudaPackages then cudaPackages.cudatoolkit_11 else cudatoolkit;
 
   luajitRev = "9143e86498436892cb4316550be4d45b68a61224";
   luajitBase = "LuaJIT-${luajitRev}";
