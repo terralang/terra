b3b4889768e6dda98f8d07cb9e7b8ff7b2b54a87
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-04-21 14:09:48 -0700

    Update CUDA installation in main CI tests too.

--- travis.sh
+++ travis.sh
@@ -78,11 +78,7 @@ if [[ $(uname) = Linux ]]; then
   fi
 
   if [[ $USE_CUDA -eq 1 ]]; then
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
-    sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-    sudo apt-get update -qq
-    sudo apt-get install -qq cuda-toolkit-9.2
+    ./docker/install_cuda.sh
   fi
 fi
 
