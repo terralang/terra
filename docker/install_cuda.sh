9991a11552ef44c74230aa0ef8531638aa877976
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-04-21 13:58:38 -0700

    Install CUDA in Docker build.

---
+++ docker/install_cuda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+apt-get update -qq
+apt-get install -qq software-properties-common
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+apt-get update -qq
+apt-get install -qq cuda-toolkit-11.6
