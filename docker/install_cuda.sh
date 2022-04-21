f637d47e23f673199c77879313327351de9cd12f
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-04-21 14:41:54 -0700

    Slim down CUDA installation by only including CUDA compiler.

--- docker/install_cuda.sh
+++ docker/install_cuda.sh
@@ -12,4 +12,4 @@ $sudo_command mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-
 $sudo_command apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 $sudo_command add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 $sudo_command apt-get update -qq
-$sudo_command apt-get install -qq cuda-toolkit-11.6
+$sudo_command apt-get install -qq cuda-compiler-11.6
