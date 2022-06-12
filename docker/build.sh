a4e11a46ad18851604cb6149e8e0ae9dbf571185
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-06-11 20:32:35 -0700

    Fix arch spelling.

--- docker/build.sh
+++ docker/build.sh
@@ -12,7 +12,9 @@ if [[ -n $arch ]]; then
     export DOCKER_BUILDKIT=1
 fi
 
-docker build ${arch:+--platform=}$arch --build-arg release=$release ${arch:+--build-arg arch=}$(echo $arch | sed -e s/arm64/aarch64/) --build-arg llvm=$llvm --build-arg threads=${threads:-4} -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro${variant:+-}$variant .
+arch_long=$(echo $arch | sed -e s/arm64/aarch64/ | sed -e s/ppc64le/powerpc64le/)
+
+docker build ${arch:+--platform=}$arch --build-arg release=$release ${arch:+--build-arg arch=}$arch_long --build-arg llvm=$llvm --build-arg threads=${threads:-4} -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro${variant:+-}$variant .
 
 # Copy files out of container and make release.
 tmp=$(docker create terralang/terra:$distro-$release)
