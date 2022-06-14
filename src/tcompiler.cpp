8a63256653ab967e2593f391d077dd652eb57e81
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-06-14 14:20:47 -0700

    Remove code for 32-bit PPC.

--- src/tcompiler.cpp
+++ src/tcompiler.cpp
@@ -823,10 +823,6 @@ struct CCallingConv {
                 return_empty_struct_as_void = true;
                 pass_struct_as_exploded_values = true;
             } break;
-            case Triple::ArchType::ppc:
-#if LLVM_VERSION >= 50
-            case Triple::ArchType::ppcle:
-#endif
             case Triple::ArchType::ppc64:
             case Triple::ArchType::ppc64le: {
                 // return_empty_struct_as_void = true;
