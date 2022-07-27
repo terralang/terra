40b08c0f27e15807b34286cb7bab30398ab444ad
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-07-27 10:01:16 -0700

    Upgrade CI to macOS 11 (#592)
    Includes new LLVM binaries for macOS 11.

--- travis.sh
+++ travis.sh
@@ -97,26 +97,26 @@ fi
 
 if [[ $(uname) = Darwin ]]; then
   if [[ $LLVM_CONFIG = llvm-config-14 ]]; then
-    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-14.0.0/clang+llvm-14.0.0-x86_64-apple-darwin.tar.xz
-    tar xf clang+llvm-14.0.0-x86_64-apple-darwin.tar.xz
-    ln -s clang+llvm-14.0.0-x86_64-apple-darwin/bin/llvm-config llvm-config-14
-    ln -s clang+llvm-14.0.0-x86_64-apple-darwin/bin/clang clang-14
-    export CMAKE_PREFIX_PATH=$PWD/clang+llvm-14.0.0-x86_64-apple-darwin
+    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-14.0.6/clang+llvm-14.0.6-x86_64-apple-darwin.tar.xz
+    tar xf clang+llvm-14.0.6-x86_64-apple-darwin.tar.xz
+    ln -s clang+llvm-14.0.6-x86_64-apple-darwin/bin/llvm-config llvm-config-14
+    ln -s clang+llvm-14.0.6-x86_64-apple-darwin/bin/clang clang-14
+    export CMAKE_PREFIX_PATH=$PWD/clang+llvm-14.0.6-x86_64-apple-darwin
   elif [[ $LLVM_CONFIG = llvm-config-13 ]]; then
-    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-13.0.0/clang+llvm-13.0.0-x86_64-apple-darwin.tar.xz
-    tar xf clang+llvm-13.0.0-x86_64-apple-darwin.tar.xz
-    ln -s clang+llvm-13.0.0-x86_64-apple-darwin/bin/llvm-config llvm-config-13
-    ln -s clang+llvm-13.0.0-x86_64-apple-darwin/bin/clang clang-13
-    export CMAKE_PREFIX_PATH=$PWD/clang+llvm-13.0.0-x86_64-apple-darwin
+    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-13.0.1/clang+llvm-13.0.1-x86_64-apple-darwin.tar.xz
+    tar xf clang+llvm-13.0.1-x86_64-apple-darwin.tar.xz
+    ln -s clang+llvm-13.0.1-x86_64-apple-darwin/bin/llvm-config llvm-config-13
+    ln -s clang+llvm-13.0.1-x86_64-apple-darwin/bin/clang clang-13
+    export CMAKE_PREFIX_PATH=$PWD/clang+llvm-13.0.1-x86_64-apple-darwin
   elif [[ $LLVM_CONFIG = llvm-config-12 ]]; then
-    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-12.0.1/clang+llvm-12.0.1-x86_64-apple-darwin.tar.xz
-    tar xf clang+llvm-12.0.1-x86_64-apple-darwin.tar.xz
+    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-12.0.1/clang+llvm-12.0.1-x86_64-apple-darwin-macos11.tar.xz
+    tar xf clang+llvm-12.0.1-x86_64-apple-darwin-macos11.tar.xz
     ln -s clang+llvm-12.0.1-x86_64-apple-darwin/bin/llvm-config llvm-config-12
     ln -s clang+llvm-12.0.1-x86_64-apple-darwin/bin/clang clang-12
     export CMAKE_PREFIX_PATH=$PWD/clang+llvm-12.0.1-x86_64-apple-darwin
   elif [[ $LLVM_CONFIG = llvm-config-11 ]]; then
-    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-11.1.0/clang+llvm-11.1.0-x86_64-apple-darwin.tar.xz
-    tar xf clang+llvm-11.1.0-x86_64-apple-darwin.tar.xz
+    curl -L -O https://github.com/terralang/llvm-build/releases/download/llvm-11.1.0/clang+llvm-11.1.0-x86_64-apple-darwin-macos11.tar.xz
+    tar xf clang+llvm-11.1.0-x86_64-apple-darwin-macos11.tar.xz
     ln -s clang+llvm-11.1.0-x86_64-apple-darwin/bin/llvm-config llvm-config-11
     ln -s clang+llvm-11.1.0-x86_64-apple-darwin/bin/clang clang-11
     export CMAKE_PREFIX_PATH=$PWD/clang+llvm-11.1.0-x86_64-apple-darwin
