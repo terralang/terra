5de1e445bb95d016de73bd83a690e22f2f633c28
Elliott Slaughter <slaughter@cs.stanford.edu>
2025-06-10 13:51:11 -0700

    Try Ubuntu 20.04.

--- docker/install_cuda.sh
+++ docker/install_cuda.sh
@@ -9,18 +9,13 @@ arch=$(uname -m | sed -e s/aarch64/arm64/)
 
 $sudo_command apt-get update -qq
 $sudo_command apt-get install -qq software-properties-common
-wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/$arch/cuda-ubuntu1804.pin
-$sudo_command mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/$arch/cuda-ubuntu2004.pin
+$sudo_command mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
-if [[ $arch = x86_64 ]]; then
-    key=3bf863cc.pub
-elif [[ $arch = arm64 ]]; then
-    key=7fa2af80.pub
-else
-    echo "Unrecognized arch $arch"
-    exit 1
-fi
-$sudo_command apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/$arch/$key
-$sudo_command add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/$arch/ /"
+# Just fetch all the keys, since they seem to reuse these between distributions.
+$sudo_command apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+$sudo_command apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+
+$sudo_command add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/$arch/ /"
 $sudo_command apt-get update -qq
-$sudo_command apt-get install -qq cuda-compiler-11.6
+$sudo_command apt-get install -qq cuda-compiler-12.2
